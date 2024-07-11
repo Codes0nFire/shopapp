2cc5c87aa19cf9d0bf52bf6fc3eb5ed7d0d8d6c1
NoN Responsive WebDeveloper <gg.georgemail50a@gmail.com>
2024-07-11 11:27:47 +0530

    created models

---
+++ models/productModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+
+
+
+const productSchema =mongoose.Schema({
+
+
+    name:String,
+    price:Number,
+    discount:{
+        type:Number,
+        defulat:0
+    },
+    image:String,
+    bgcolor:String,
+    panelcolor:String,
+    textcolor:String
+
+
+})
+
+
+
+module.exports =mongoose.model("product",productSchema);
---
+++ models/userModel.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/shopapp");
+
+
+const userSchema =mongoose.Schema({
+
+
+    fullname:String,
+    email:String,
+    password:String,
+    isadmin:Boolean,
+    contact:Number,
+    picture: String,
+    orders:{
+        type:Array,
+        default:[]
+    },
+
+    cart:{
+        type:Array,
+        default:[]
+    }
+
+
+
+
+})
+
+
+
+module.exports =mongoose.model("user",userSchema);
